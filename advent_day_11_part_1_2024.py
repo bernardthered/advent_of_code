c55817abec7d5b6eb07b5ac7287cc817f7636de2
bernardthered <bernardthered@gmail.com>
2024-12-16 17:24:36 -0800

    Semi-working part 2
    It works for the 2 simpler maps, but not for the 2 more complex ones.

--- advent_day_11_part_1_2024.py
+++ advent_day_11_part_1_2024.py
@@ -1,8 +1,14 @@
 # Solution to
 # https://adventofcode.com/2024/day/12
 
+DIRECTIONS = {0: "up", 1: "right", 2: "down", 3: "left"}
 
-garden_map = open("day_11_input.txt").read()
+
+# garden_map = open("day_11_input.txt").read()
+garden_map = """AAAA
+BBCD
+BBCC
+EEEC"""
 highest_row_num = 0
 highest_col_num = 0
 
@@ -10,6 +16,13 @@ highest_col_num = 0
 def log(msg):
     if False:
         print(msg)
+
+
+class GardenSquare(str):
+
+    def __init__(self, _):
+        # The 4 elements represent whether there is a known side above, right, below, left of this square
+        self.sides = [False, False, False, False]
 
 
 def convert_map_to_array(garden):
@@ -21,19 +34,49 @@ def convert_map_to_array(garden):
     for row_num, row in enumerate(garden.split("\n")):
         garden_rows.append([])
         for letter in row:
-            garden_rows[row_num].append(letter)
+            garden_rows[row_num].append(GardenSquare(letter))
     highest_row_num = row_num
     highest_col_num = len(garden_rows[0]) - 1
     return garden_rows
 
 
 def get_adjacent_squares(row_num, col_num):
+    # Return the squares up, right, down, and left
+    # (starting at north and going clockwise)
     return [
-        (row_num + 1, col_num),
         (row_num - 1, col_num),
         (row_num, col_num + 1),
+        (row_num + 1, col_num),
         (row_num, col_num - 1),
     ]
+
+
+def is_off_the_map(square):
+    row_num, col_num = square
+    return (
+        row_num < 0
+        or col_num < 0
+        or row_num > highest_row_num
+        or col_num > highest_col_num
+    )
+
+
+def is_new_side(garden_rows, adjacent_squares, direction, this_veggie):
+    orthogonal_direction = (direction - 1) % 4
+    orthogonal_square = adjacent_squares[orthogonal_direction]
+
+    if not (is_off_the_map(orthogonal_square)):
+        orthogonal_veggie = garden_rows[orthogonal_square[0]][orthogonal_square[1]]
+        if orthogonal_veggie == this_veggie and orthogonal_veggie.sides[direction]:
+            return False
+
+    orthogonal_direction = (direction + 1) % 4
+    orthogonal_square = adjacent_squares[orthogonal_direction]
+    if not (is_off_the_map(orthogonal_square)):
+        orthogonal_veggie = garden_rows[orthogonal_square[0]][orthogonal_square[1]]
+        if orthogonal_veggie == this_veggie and orthogonal_veggie.sides[direction]:
+            return False
+    return True
 
 
 def map_region(garden_rows, visited, row_num, col_num) -> tuple[int, int]:
@@ -46,43 +89,50 @@ def map_region(garden_rows, visited, row_num, col_num) -> tuple[int, int]:
     area = 1  # This square
     this_veggie = garden_rows[row_num][col_num]
     perimeter = 0
+    sides = 0
     visited.add((row_num, col_num))
 
-    for adjacent_row_num, adjacent_col_num in get_adjacent_squares(row_num, col_num):
-        # if it's off the map, add one to the perimeter and continue
+    adjacent_squares = get_adjacent_squares(row_num, col_num)
+
+    for direction in DIRECTIONS:
+        adjacent_square = adjacent_squares[direction]
+        adjacent_row_num, adjacent_col_num = adjacent_square
+        # if it's off the map or a different veggie, add one to the perimeter and continue
         if (
-            adjacent_row_num < 0
-            or adjacent_col_num < 0
-            or adjacent_row_num > highest_row_num
-            or adjacent_col_num > highest_col_num
+            is_off_the_map(adjacent_square)
+            or (adjacent_veggie := garden_rows[adjacent_row_num][adjacent_col_num])
+            != this_veggie
         ):
             perimeter += 1
+            this_veggie.sides[direction] = True
+            # check if this is a new side that we are starting
+            if is_new_side(garden_rows, adjacent_squares, direction, this_veggie):
+                sides += 1
             continue
-
-        adjacent_veggie = garden_rows[adjacent_row_num][adjacent_col_num]
         log(
             f"    Looking at adjacent square of {adjacent_veggie} ({adjacent_col_num}, {adjacent_row_num})"
         )
-        # if it's a diff veggie, add one to the perimeter and continue
-        if adjacent_veggie != this_veggie:
-            perimeter += 1
-            continue
-
         # If it's already been visited, continue w/o adding to perimeter or area
         if (adjacent_row_num, adjacent_col_num) in visited:
             continue
 
         # It's the same veggie! Recurse, adding the adjacent square's area & perimeter to our own
-        adjacent_area, adjacent_perimeter = map_region(
+        adjacent_area, adjacent_perimeter, adjacent_sides = map_region(
             garden_rows, visited, adjacent_row_num, adjacent_col_num
         )
         area += adjacent_area
         perimeter += adjacent_perimeter
+        sides += adjacent_sides
 
-    return area, perimeter
+    return area, perimeter, sides
 
 
-def compute_cost_of_garden(garden):
+def compute_cost_of_garden(garden, style=1):
+    """
+    If parameter 'part' is 1, the cost of each region will be area x perimeter.
+    If parameter 'part' is 2, the cost of each region will be area x # of sides.
+    """
+
     global highest_row_num
     global highest_col_num
     visited = set()
@@ -97,12 +147,16 @@ def compute_cost_of_garden(garden):
 
             veggie = garden_rows[row_num][col_num]
             log(f"Processing region of {veggie} starting at {col_num}, {row_num}")
-            area, perimeter = map_region(garden_rows, visited, row_num, col_num)
+            area, perimeter, sides = map_region(garden_rows, visited, row_num, col_num)
+            if style == 1:
+                cost = area * perimeter
+            else:
+                cost = area * sides
             print(
-                f"Found a region of {veggie} of area {area} and perimeter {perimeter}, making its fencing cost {area * perimeter}."
+                f"Found a region of {veggie} of area {area}, perimeter {perimeter}, and {sides} sides, making its fencing cost ${cost}."
             )
             total_area += area
-            total_cost += area * perimeter
+            total_cost += cost
 
     garden = garden.replace("\n", "")
     if total_area != len(garden):
@@ -114,4 +168,4 @@ def compute_cost_of_garden(garden):
 
 
 if __name__ == "__main__":
-    compute_cost_of_garden(garden_map)
+    compute_cost_of_garden(garden_map, 2)
--- test_advent_day_11_part_1_2024.py
+++ test_advent_day_11_part_1_2024.py
@@ -3,7 +3,7 @@ import unittest
 from advent_day_11_part_1_2024 import compute_cost_of_garden
 
 
-sample_data = [
+sample_data_part1 = [
     (
         """AAAA
 BBCD
@@ -34,8 +34,52 @@ MMMISSJEEE""",
     ),
 ]
 
+sample_data_part2 = [
+    (
+        """AAAA
+BBCD
+BBCC
+EEEC""",
+        80,
+    ),
+    (
+        """EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE""",
+        236,
+    ),
+    (
+        """AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA""",
+        368,
+    ),
+    (
+        """RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE""",
+        1206,
+    ),
+]
+
 
 class TestComputeCostOfGargen(unittest.TestCase):
-    def test_with_sample_data(self):
-        for garden, expected_cost in sample_data:
+    def test_part1_with_sample_data(self):
+        for garden, expected_cost in sample_data_part1:
             self.assertEqual(compute_cost_of_garden(garden), expected_cost)
+
+    def test_part2_with_sample_data(self):
+        for garden, expected_cost in sample_data_part2:
+            self.assertEqual(compute_cost_of_garden(garden, style=2), expected_cost)
