2d5aca3bccb9125cd6a965dd42695054b4a3928a
bernardthered <bernardthered@gmail.com>
2024-12-22 08:03:42 -0800

    Day 21 part 2 maybe working but too slow

--- 2024/day21/keypad_conundrum.py
+++ 2024/day21/keypad_conundrum.py
@@ -1,33 +1,37 @@
-# Solution to https://adventofcode.com/2024/day/2
+# Solution to https://adventofcode.com/2024/day/21
 
-import copy
+import functools
 import os
 import re
+import time
 
 INPUT_FILE_NUMBER = 2
+# For ppart 1, this should be 3, for part 2, it should be 26
+DEPTH_OF_ROBOTS = 26
 
-keypad = {
-    "7": (0, 0),
-    "8": (0, 1),
-    "9": (0, 2),
-    "4": (1, 0),
-    "5": (1, 1),
-    "6": (1, 2),
-    "1": (2, 0),
-    "2": (2, 1),
-    "3": (2, 2),
-    "0": (3, 1),
-    "A": (3, 2),
-    "verboten": (3, 0),
-}
-
-directional_pad = {
-    "^": (0, 1),
-    "A": (0, 2),
-    "<": (1, 0),
-    "v": (1, 1),
-    ">": (1, 2),
-    "verboten": (0, 0),
+PADS = {
+    "keypad": {
+        "7": (0, 0),
+        "8": (0, 1),
+        "9": (0, 2),
+        "4": (1, 0),
+        "5": (1, 1),
+        "6": (1, 2),
+        "1": (2, 0),
+        "2": (2, 1),
+        "3": (2, 2),
+        "0": (3, 1),
+        "A": (3, 2),
+        "verboten": (3, 0),
+    },
+    "directional_pad": {
+        "^": (0, 1),
+        "A": (0, 2),
+        "<": (1, 0),
+        "v": (1, 1),
+        ">": (1, 2),
+        "verboten": (0, 0),
+    },
 }
 
 
@@ -51,12 +55,10 @@ def move_vertically(l1, l2):
     return "v" * abs(Δy)
 
 
-def get_sequence_of_button_presses_for_single_move(char1, char2, pad):
-    # This approach is insufficient for 1+ reasons:
-    #   1) the robot cannot move its finger over the blank space on the keypad
-    #   2) to optimize for shortest path, we may need to strategically end the intermediary
-    #      robot arms over one button instead of another. Perhaps this doesn't matter, since
-    #      they need to move to the A after every set of moves. Needs further consideration.
+@functools.cache
+def get_button_presses_for_move(char1, char2, padname) -> str:
+    pad = PADS[padname]
+
     sequence = ""
     l1 = pad[char1]
     l2 = pad[char2]
@@ -83,42 +85,45 @@ def get_sequence_of_button_presses_for_single_move(char1, char2, pad):
     return sequence + "A"
 
 
-def get_sequence_of_button_presses_for_code(code, pad):
+def get_button_presses_for_code(code, pad):
     sequence = ""
     startingchar = "A"
     for char in code:
-        sequence += get_sequence_of_button_presses_for_single_move(
-            startingchar, char, pad
-        )
+        sequence += get_button_presses_for_move(startingchar, char, pad)
         startingchar = char
-        # sequence += get_sequence_of_button_presses_for_single_move(char, "A", keypad)
     # print(f"Code: {code}, seq: {sequence}")
     return sequence
 
 
-def get_code_complexity(code):
-    first_directional_pad_sequence = get_sequence_of_button_presses_for_code(
-        code, keypad
-    )
-    second_directional_pad_sequence = get_sequence_of_button_presses_for_code(
-        first_directional_pad_sequence, directional_pad
-    )
-    third_directional_pad_sequence = get_sequence_of_button_presses_for_code(
-        second_directional_pad_sequence, directional_pad
-    )
+def get_outer_sequence(code, levels_deep):
+    t1 = time.time()
+    current_pad_sequence = get_button_presses_for_code(code, "keypad")
+    for i in range(levels_deep - 1):
+        td = int(time.time() - t1)
+        print(
+            f"{i+1}/{levels_deep - 1}, {td}s: current length = {len(current_pad_sequence)}"
+        )
+        current_pad_sequence = get_button_presses_for_code(
+            current_pad_sequence, "directional_pad"
+        )
+    return current_pad_sequence
+
+
+def get_outer_code_complexity(code):
+    outer_sequence = get_outer_sequence(code, DEPTH_OF_ROBOTS)
     numeric_part_of_code = re.sub("^0*", "", code)
     numeric_part_of_code = re.sub("A", "", numeric_part_of_code)
-    complexity = len(third_directional_pad_sequence) * int(numeric_part_of_code)
-    print(
-        f"Code: {code}, {len(third_directional_pad_sequence)} * {int(numeric_part_of_code)}, {third_directional_pad_sequence}"
-    )
+    complexity = len(outer_sequence) * int(numeric_part_of_code)
+    # print(
+    #     f"Code: {code}, {len(outer_sequence)} * {int(numeric_part_of_code)}, {outer_sequence}"
+    # )
     return complexity
 
 
 def get_code_complexities(codes):
     answer = 0
     for code in codes:
-        answer += get_code_complexity(code.strip())
+        answer += get_outer_code_complexity(code.strip())
     return answer
 
 
