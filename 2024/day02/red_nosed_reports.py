f66610e91b29bfb71dcd51790a4bb57d0e89a6e7
bernardthered <bernardthered@gmail.com>
2024-12-21 17:22:14 -0800

    Day 2 part 2

--- 2024/day02/red_nosed_reports.py
+++ 2024/day02/red_nosed_reports.py
@@ -1,8 +1,9 @@
 # Solution to https://adventofcode.com/2024/day/2
 
+import copy
 import os
 
-INPUT_FILE_NUMBER = 1
+INPUT_FILE_NUMBER = 2
 
 
 def parse_input():
@@ -11,8 +12,7 @@ def parse_input():
     return input_file
 
 
-def is_report_safe(report):
-    levels = [int(level) for level in report.split()]
+def is_report_safe(levels):
     increasing = False
     if levels[1] > levels[0]:
         increasing = True
@@ -21,30 +21,30 @@ def is_report_safe(report):
         if increasing:
             if diff > 3 or diff < 1:
                 # This report is unsafe, no further processing needed
-                # print(
-                #     f"Unsafe increasing report: {levels} ({levels[i]} - {levels[i - 1]} = {diff})"
-                # )
                 return False
         else:
             if diff < -3 or diff > -1:
                 # This report is unsafe, no further processing needed
-                # print(
-                #     f"Unsafe decreasing report: {levels} ({levels[i]} - {levels[i - 1]} = {diff})"
-                # )
                 return False
-    # print(f"Safe: {levels}")
     return True
 
 
 def count_safe_reports(input_file):
     safe_count = 0
     for report in input_file.readlines():
-        if is_report_safe(report):
+        levels = [int(level) for level in report.split()]
+        if is_report_safe(levels):
             safe_count += 1
+        else:
+            for i in range(len(levels)):
+                revised_levels = copy.copy(levels)
+                del revised_levels[i]
+                if is_report_safe(revised_levels):
+                    safe_count += 1
+                    break
     return safe_count
 
 
 if __name__ == "__main__":
     input_file = parse_input()
-    # print(compute_distance_between_lists(l1, l2))
     print(count_safe_reports(input_file))
