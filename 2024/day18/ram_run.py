2ad7d25145d7724afa58ecb755001242794218af
bernardthered <bernardthered@gmail.com>
2024-12-27 11:07:30 -0800

    Day 18 parts 1 & 2

--- 2024/day18/ram_run.py
+++ 2024/day18/ram_run.py
@@ -1,36 +1,26 @@
 # Solution to
 # https://adventofcode.com/2024/day/18
 
-import copy
-from functools import cache
-import math
 import os
 
-LOGLEVEL = 100
-
-DIRECTIONS = {0: "up", 1: "right", 2: "down", 3: "left"}
-NUM_ROWS_AND_COLS = 71
-
+INPUT_FILENUMBER = NUM_ROWS_AND_COLS = 7
+MAX_BYTES = 12
+INPUT_FILENUMBER = NUM_ROWS_AND_COLS = 71
+MAX_BYTES = 1024
 
 map = []
-min_steps_to_exit_from_location = {}
 
 
-class InfiniteLoop(Exception):
+class NoRouteExists(Exception):
     pass
 
 
-def log(msg, level=10):
-    if level >= LOGLEVEL:
-        print(msg)
-
-
-def construct_map(max_bytes=1024):
+def construct_map(max_bytes=MAX_BYTES) -> list[str]:
     global map
     map = [[0 for i in range(NUM_ROWS_AND_COLS)] for j in range(NUM_ROWS_AND_COLS)]
 
     script_path = os.path.dirname(os.path.realpath(__file__))
-    input = open(os.path.join(script_path, "input_2.txt"))
+    input = open(os.path.join(script_path, f"input_{INPUT_FILENUMBER}.txt"))
     lines = input.readlines()
     for i, line in enumerate(lines):
         if i >= max_bytes:
@@ -38,7 +28,7 @@ def construct_map(max_bytes=1024):
 
         x, y = line.strip().split(",")
         map[int(x)][int(y)] = 1
-    return map
+    return lines[i:]
 
 
 def print_map(previously_visited=None):
@@ -54,60 +44,67 @@ def print_map(previously_visited=None):
         print()
 
 
-def is_off_the_map(col_num, row_num):
-    return (
-        row_num < 0
-        or col_num < 0
-        or row_num >= NUM_ROWS_AND_COLS
-        or col_num >= NUM_ROWS_AND_COLS
-    )
+def get_adjacent_empty_squares(location, visited: set) -> set:
+    adjacent_empty_squares = set()
+    x, y = location
+    if x > 0:
+        if map[x - 1][y] == 0:
+            adjacent_empty_squares.add((x - 1, y))
+    if x < NUM_ROWS_AND_COLS - 1:
+        if map[x + 1][y] == 0:
+            adjacent_empty_squares.add((x + 1, y))
+    if y > 0:
+        if map[x][y - 1] == 0:
+            adjacent_empty_squares.add((x, y - 1))
+    if y < NUM_ROWS_AND_COLS - 1:
+        if map[x][y + 1] == 0:
+            adjacent_empty_squares.add((x, y + 1))
+    adjacent_empty_squares -= visited
+    return adjacent_empty_squares
 
 
-def next_square(location: tuple[int, int], direction: int) -> tuple[int, int]:
-    match direction:
-        case 0:
-            return location[0], location[1] - 1
-        case 1:
-            return location[0] + 1, location[1]
-        case 2:
-            return location[0], location[1] + 1
-        case 3:
-            return location[0] - 1, location[1]
+def find_min_distance_between_two_points(
+    start=(0, 0), end=(NUM_ROWS_AND_COLS - 1, NUM_ROWS_AND_COLS - 1)
+) -> int:
+    next_step_locations = set([start])
+    steps_from_start = 0
+    visited = set()
+
+    while True:
+        if not next_step_locations:
+            raise NoRouteExists(f"Couldn't find a path, after {steps_from_start} steps")
+        steps_from_start += 1
+
+        locations = next_step_locations.copy()
+        next_step_locations = set()
+        for location in locations:
+            next_step_locations |= get_adjacent_empty_squares(location, visited)
+            visited.add(location)
+
+        if end in next_step_locations:
+            # print(f"Found a path to {end}!")
+            # print_map(visited)
+            return steps_from_start
 
 
-def find_path_to_exit(location=(0, 0), steps_so_far=0, previously_visited=None) -> list:
-    global map
+def part1():
+    construct_map()
+    print(find_min_distance_between_two_points())
 
-    if location in min_steps_to_exit_from_location:
-        return min_steps_to_exit_from_location[location]
 
-    if location == (NUM_ROWS_AND_COLS - 1, NUM_ROWS_AND_COLS - 1):
-        min_steps_to_exit_from_location[location] = steps_so_far
-        return steps_so_far
-
-    min_steps_to_exit = math.inf
-    if previously_visited is None:
-        previously_visited = set()
-
-    for direction in DIRECTIONS:
-        next_location = next_square(location, direction)
-        if next_location in previously_visited or is_off_the_map(*next_location):
-            continue
-        next_terrain = map[next_location[0]][next_location[1]]
-        match next_terrain:
-            case 0:
-                previously_visited.add(location)
-                steps_to_exit = find_path_to_exit(
-                    next_location, steps_so_far + 1, previously_visited.copy()
-                )
-                if steps_to_exit < min_steps_to_exit:
-                    min_steps_to_exit = steps_to_exit
-            case 1:
-                continue
-    return min_steps_to_exit
+def part2():
+    # Brute force approach, but it works and only takes a few seconds
+    # with the large dataset
+    remaining_bytes = construct_map()
+    for byte in remaining_bytes:
+        x, y = byte.strip().split(",")
+        map[int(x)][int(y)] = 1
+        try:
+            find_min_distance_between_two_points()
+        except NoRouteExists:
+            print(f"{x},{y}")
+            return
 
 
 if __name__ == "__main__":
-    construct_map()
-    print_map()
-    print(find_path_to_exit())
+    part2()
