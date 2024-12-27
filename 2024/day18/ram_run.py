1213b10944b15d8ffe6e5bf6239c20ddbe6ef407
bernardthered <bernardthered@gmail.com>
2024-12-27 11:14:27 -0800

    Day 18 code cleanup

--- 2024/day18/ram_run.py
+++ 2024/day18/ram_run.py
@@ -3,8 +3,10 @@
 
 import os
 
+# part 1
 INPUT_FILENUMBER = NUM_ROWS_AND_COLS = 7
 MAX_BYTES = 12
+# part 2
 INPUT_FILENUMBER = NUM_ROWS_AND_COLS = 71
 MAX_BYTES = 1024
 
@@ -66,6 +68,20 @@ def get_adjacent_empty_squares(location, visited: set) -> set:
 def find_min_distance_between_two_points(
     start=(0, 0), end=(NUM_ROWS_AND_COLS - 1, NUM_ROWS_AND_COLS - 1)
 ) -> int:
+    """
+    A breadth-first search of the map, taking one step at a time and then
+    adding all the unvisited adjacent open squares to the list of squares to
+    process after taking the following step.
+
+    This avoids the cost of recursion and guarantees that we know the minimum
+    number of steps to get to each square the first time we hit that square,
+    so there is never a need to revisit any visited square. That in turn allows
+    returning from this method as soon as we reach the end square, even if there
+    are unprocessed paths and steps (because we know they won't get to the
+    endpoint any sooner).
+
+    Return the minimum number of steps to get from start to end.
+    """
     next_step_locations = set([start])
     steps_from_start = 0
     visited = set()
@@ -82,8 +98,6 @@ def find_min_distance_between_two_points(
             visited.add(location)
 
         if end in next_step_locations:
-            # print(f"Found a path to {end}!")
-            # print_map(visited)
             return steps_from_start
 
 
