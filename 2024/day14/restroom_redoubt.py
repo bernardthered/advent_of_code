f917428fdb7ce3367f521fc0686c7ea8e7a1f252
bernardthered <bernardthered@gmail.com>
2025-01-09 11:03:15 -0800

    Day 14 part 2

--- 2024/day14/restroom_redoubt.py
+++ 2024/day14/restroom_redoubt.py
@@ -40,7 +40,7 @@ def add_to_quadrant_count(x, y, width, height, quadrants):
     return quadrants
 
 
-if __name__ == "__main__":
+def part1():
     # width = 11
     # height = 7
     width = 101
@@ -55,3 +55,57 @@ if __name__ == "__main__":
             quadrants = add_to_quadrant_count(x, y, width, height, quadrants)
     print(quadrants)
     print(math.prod(quadrants))
+
+
+def print_map(final_positions, width, height):
+    for y in range(height):
+        for x in range(width):
+            if (x, y) in final_positions:
+                print("O", end="")
+            else:
+                print(".", end="")
+        print()
+
+
+def is_horizontal_line_in_positions(final_positions, x, y, length):
+    """
+    Check from position x,y to see if there is a line that forms either direction of at least length 'length'
+    """
+    for i in range(length):
+        if (x - i, y) not in final_positions:
+            break
+    # At this point, we have moved as far left as the line goes. We'll try moving right from the starting point next
+    # if the total number of spaces that the line goes left (i) plus the number right it goes (j) gets to 'length',
+    # then we found a line of that length.
+    for j in range(i, length):
+        if (x + j, y) not in final_positions:
+            return False
+    return True
+
+
+def part2():
+    width = 11
+    height = 7
+    width = 101
+    height = 103
+    ps_and_vs = []
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(script_path, "input_2.txt")) as inputfile:
+        for line in inputfile.readlines():
+            x, y, vx, vy = parse_input_line(line)
+            ps_and_vs.append((x, y, vx, vy))
+
+    s = 0
+    while True:
+        s += 1
+        final_positions = set()
+        for x, y, vx, vy in ps_and_vs:
+            x, y = compute_final_position(x, y, vx, vy, width, height, s)
+            final_positions.add((x, y))
+            if is_horizontal_line_in_positions(final_positions, x, y, 10):
+                print_map(final_positions, width, height)
+                input(f"{s}s elapsed. press enter to continue.")
+
+
+if __name__ == "__main__":
+    part2()
